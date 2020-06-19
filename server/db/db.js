a7e97db4fbf3c1834b3d0809e96fb074eb73378f
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-06-19 03:03:25 -0400

    set up pool for pg

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,15 @@
-const Pool = require('pq').Pool;
-const env
+const Pool = require('pg').Pool;
+
+const { PG_USER, PG_PASSWPRD, PG_API, PG_PORT, PG_DB } = process.env;
+
 
 const pool = new Pool({
-  user: ''
+  user: PG_USER,
+  password: PG_PASSWPRD,
+  host: PG_API,
+  port: PG_PORT,
+  database: PG_DB
 })
+
+
+module.exports = pool;
