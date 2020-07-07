d7945a2ccc990d239ae292f309e23ee9a8a53431
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-07-06 21:43:15 -0400

    successfully display other user's profile page with their habits

--- client/src/views/UserPage/UserPage.js
+++ client/src/views/UserPage/UserPage.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { UserCard } from '../../components/';
 /** Styles **/
 import './UserPage.css';
+import HabitCard from '../../components/HabitCard/HabitCard';
 
 // This view is used to display another user's page (not current user)
 
@@ -12,10 +13,10 @@ const UserPage = () => {
   const [userObj, setUserObj] = useState([]);
   const [userHabits, setUserHabits] = useState([]);
 
-  useEffect(() => {
-    // get user_id from browser window
-    const user_id = window.location.href.split('users/')[1];
+  // get user_id from browser window
+  const user_id = window.location.href.split('users/')[1];
 
+  useEffect(() => {
     getUserInfo(user_id);
     getHabits(user_id);
   }, []);
@@ -38,13 +39,24 @@ const UserPage = () => {
   const getHabits = async user_id => {
     console.log('Fetching user habits for:', user_id);
     const endpoint = process.env.REACT_APP_API +
-      ``;
+      `habits/${user_id}`;
+    
+    axios
+      .get(endpoint)
+      .then(res => {
+        console.log('This user habits', res.data);
+        setUserHabits(res.data);
+      })
+      .catch(err => console.log(err));
   };
 
 
   return (
     <section className="UserPage">
       {userObj.map(user => (<UserCard userObj={user} />))}
+      {userHabits.map(habit => (
+        <HabitCard key={habit.habit_id} habit={habit} />
+      ))}
     </section>
   )
 }
