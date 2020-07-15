c0ddcf6b3b64e488bf8e7bb2307ce5dd7d5a6275
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-07-15 00:18:38 -0400

    added page displaying followers

--- client/src/views/FollowingPage/FollowingPage.js
+++ client/src/views/FollowingPage/FollowingPage.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+/** Helpers **/
+import { getMyFollows, getUserFollowers } from '../../helpers/followDataHelpers';
+import { getUserAPIData } from '../../helpers/getDataHelpers';
+/** Subcomponents **/
+import { UserCard } from '../../components';
+/** Redux **/
+import { useSelector } from 'react-redux';
+
 
 const FollowingPage = () => {
+  /** State **/
+  const [users, setUsers] = useState([]);
+
+  /** Redux **/
+  const counter = useSelector(state => state.counter);
+
+  // get a list of users the user follows
+  useEffect(() => {
+    getMyFollows(data => {
+      console.log(data)
+      data.forEach(user => {
+        console.log(user)
+        getUserAPIData(user.target_user_id, userObj => {
+          setUsers(prev => [...prev, userObj]);
+        });
+      });
+    });
+  }, [counter]);
+
+  // map for rendering
+  const _users = users.map(userObj => {
+    return (
+      <UserCard 
+        key={userObj.user_id}
+        userObj={userObj} />
+    );
+  });
+
   return (
     <div className="FollowingPage navbar-offset">
-      following page
-      <div> wtf</div>
-      <div> wtf</div>
-      <div> wtf</div>
-      <div> wtf</div>
-    </div>
-  )
-}
+      <h5>
+        Here are the users you follow!
+      </h5>
 
-export default FollowingPage
+      {_users}
+    </div>
+  );
+};
+
+export default FollowingPage;
