10919219b6a3596e1a6fc41544439a9b1b6b5ff3
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-07-16 22:28:08 -0400

    fetch user's followers

--- client/src/views/FollowersPage/FollowersPage.js
+++ client/src/views/FollowersPage/FollowersPage.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+/** Helpers **/
+import { getUserFollowers } from '../../helpers/followDataHelpers';
 /** Styles **/
 import './FollowersPage.css';
 
 const FollowersPage = () => {
+  /** State **/
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUserFollowers(1, (data, err) => {
+      if (err) return console.log(err);
+      console.log('Data!', data);
+      setUsers(data);
+    });
+  }, []);
+
   return (
     <section className="navbar-offset">
       followers page
