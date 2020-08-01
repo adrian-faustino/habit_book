4d79ae3573c0c0d6bd1be43c4ace9d3306f4b754
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-08-01 18:15:02 -0400

    removing test page for production

--- client/src/components/Navbar/NavDashboard.js
+++ client/src/components/Navbar/NavDashboard.js
@@ -1,29 +1,24 @@
-import React from 'react'
+import React from "react";
 /** Redux **/
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 /** Redux actions */
-import { clearUser } from '../../actions/userActions';
-import { logout } from '../../actions';
+import { clearUser } from "../../actions/userActions";
+import { logout } from "../../actions";
 /** Reactstrap */
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
 /** React router **/
-import { useHistory } from 'react-router';
+import { useHistory } from "react-router";
 /** Styles **/
-import './NavDashboard.css';
+import "./NavDashboard.css";
 
-const NavDashboard = props => {
+const NavDashboard = (props) => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.user);
-  const {
-    first_name,
-    last_name,
-    username,
-    email
-  } = user;
+  const user = useSelector((state) => state.user);
+  const { first_name, last_name, username, email } = user;
 
   const { push } = useHistory();
 
-  const handleLogout = e => {
+  const handleLogout = (e) => {
     e.preventDefault();
     localStorage.clear();
     dispatch(logout());
@@ -33,31 +28,27 @@ const NavDashboard = props => {
     window.location.reload();
 
     // trigger view change
-    push('/');
+    push("/");
   };
 
   return (
-    <div
-      className="NavDashboard__container">
-      <span 
-        className="NavDashboard__welcome-span">
+    <div className="NavDashboard__container">
+      <span className="NavDashboard__welcome-span">
         {`Welcome, ${first_name}`}
       </span>
 
-
-      <Button
+      {/* for development only  */}
+      {/* <Button
         className="NavDashboard__button"
         onClick={() => console.log(localStorage)}>
         localstorage
-      </Button>
+      </Button> */}
 
-      <Button
-        className="NavDashboard__button"
-        onClick={handleLogout}>
+      <Button className="NavDashboard__button" onClick={handleLogout}>
         logout
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default NavDashboard
+export default NavDashboard;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,87 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 /** Reactstrap **/
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink } from "reactstrap";
 /** React router **/
-import { Link, Redirect } from 'react-router-dom'; 
+import { Link, Redirect } from "react-router-dom";
 /** Styles **/
-import './Navbar.css';
-import NavDashboard from './NavDashboard';
-
+import "./Navbar.css";
+import NavDashboard from "./NavDashboard";
 
 const Navbar = () => {
-  const isLogged = useSelector(state => state.isLogged);
+  const isLogged = useSelector((state) => state.isLogged);
 
   return (
     <Nav className="Navbar__container">
-      {isLogged && (<> 
-        <NavItem>
-          <NavLink
-            className="Navbar__li"
-            tag={Link}
-            to="/home">
+      {isLogged && (
+        <>
+          <NavItem>
+            <NavLink className="Navbar__li" tag={Link} to="/home">
               Home
-          </NavLink>
-        </NavItem>
+            </NavLink>
+          </NavItem>
 
-        <NavItem>
+          {/* for development only - test buttons */}
+          {/* <NavItem>
           <NavLink
             className="Navbar__li"
             tag={Link}
             to="/test">
               Test
           </NavLink>
-        </NavItem>  
+        </NavItem>   */}
 
-        <NavItem>
-          <NavLink
-            className="Navbar__li"
-            tag={Link}
-            to="/search-users">
+          <NavItem>
+            <NavLink className="Navbar__li" tag={Link} to="/search-users">
               Find Users
-          </NavLink>
-        </NavItem>
+            </NavLink>
+          </NavItem>
 
-        <NavItem>
-          <NavLink
-            className="Navbar__li"
-            tag={Link}
-            to="/my-followers">
+          <NavItem>
+            <NavLink className="Navbar__li" tag={Link} to="/my-followers">
               My Followers
-          </NavLink>
-        </NavItem>
+            </NavLink>
+          </NavItem>
 
-        <NavItem>
-          <NavLink
-            className="Navbar__li"
-            tag={Link}
-            to="/following">
+          <NavItem>
+            <NavLink className="Navbar__li" tag={Link} to="/following">
               Following
-          </NavLink>
-        </NavItem>
-      </>)}
-      
-      {!isLogged && (<>
-        <NavItem>
-          <NavLink
-            className="Navbar__li"
-            tag={Link}
-            to="/login">
-              Login
-          </NavLink>
-        </NavItem>
+            </NavLink>
+          </NavItem>
+        </>
+      )}
 
-        <NavItem>
-          <NavLink
-            className="Navbar__li"
-            tag={Link}
-            to="/signup">
+      {!isLogged && (
+        <>
+          <NavItem>
+            <NavLink className="Navbar__li" tag={Link} to="/login">
+              Login
+            </NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink className="Navbar__li" tag={Link} to="/signup">
               Register
-          </NavLink>
-        </NavItem>
-      </>)}
+            </NavLink>
+          </NavItem>
+        </>
+      )}
 
       {isLogged && (
         <NavItem>
@@ -93,8 +78,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
 
 /* React-router with Reactstrap notes:
  * - Use tag attribute to prevent page reload when changing page */
