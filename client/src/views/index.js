4b1e116fdf3d5eec8b8820b6bbd95245cd21326d
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-08-07 18:32:09 -0400

    deploying

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,9 +1,8 @@
-export { default as SignUpPage } from './SignUpPage/SignUpPage';
-export { default as WelcomePage } from './WelcomePage/WelcomePage';
-export { default as HomePage } from './HomePage/HomePage';
-export { default as LoginPage } from './LoginPage/LoginPage';
-export { default as LandingPage } from './LandingPage/LandingPage';
-export { default as SearchPage } from './SearchPage/SearchPage';
-export { default as UserPage } from './UserPage/UserPage';
-export { default as FollowingPage } from './FollowingPage/FollowingPage';
-export { default as FollowersPage } from './FollowersPage/FollowersPage';
+export { default as SignUpPage } from "./SignUpPage/SignUpPage";
+export { default as WelcomePage } from "./WelcomePage/WelcomePage";
+export { default as HomePage } from "./HomePage/HomePage";
+export { default as LoginPage } from "./LoginPage/LoginPage";
+export { default as SearchPage } from "./SearchPage/SearchPage";
+export { default as UserPage } from "./UserPage/UserPage";
+export { default as FollowingPage } from "./FollowingPage/FollowingPage";
+export { default as FollowersPage } from "./FollowersPage/FollowersPage";
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const path = require("path");
 
 /** Constants **/
 const PORT = process.env.PORT;
@@ -10,24 +11,28 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV !== "production") {
+  const absPath = path.join(__dirname, "../client/build");
+  app.use(express.static(absPath));
+}
+
 /** Routes **/
-const homeRoutes = require('./routes');
-const userRoutes = require('./routes/userCRUD');
-const userAuth = require('./routes/userAuth');
-const habitRoutes = require('./routes/habitCRUD');
-const completed_atRoutes = require('./routes/completed_atCRUD');
-const commentRoutes = require('./routes/commentsCRUD');
-const userSearch = require('./routes/userSearch');
-const followRoutes = require('./routes/followCRUD');
+const homeRoutes = require("./routes");
+const userRoutes = require("./routes/userCRUD");
+const userAuth = require("./routes/userAuth");
+const habitRoutes = require("./routes/habitCRUD");
+const completed_atRoutes = require("./routes/completed_atCRUD");
+const commentRoutes = require("./routes/commentsCRUD");
+const userSearch = require("./routes/userSearch");
+const followRoutes = require("./routes/followCRUD");
 
-app.use('/login', userAuth);
-app.use('/users', userRoutes);
-app.use('/habits', habitRoutes);
-app.use('/completed_at', completed_atRoutes);
-app.use('/search', userSearch);
-app.use('/comments', commentRoutes);
-app.use('/follows', followRoutes);
-app.use('/', homeRoutes);
-
+app.use("/login", userAuth);
+app.use("/users", userRoutes);
+app.use("/habits", habitRoutes);
+app.use("/completed_at", completed_atRoutes);
+app.use("/search", userSearch);
+app.use("/comments", commentRoutes);
+app.use("/follows", followRoutes);
+app.use("/", homeRoutes);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
